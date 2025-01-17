27f1bbf50b7858a16e75e1508f4972cea79e243c
Shiv Kumar <shivkumartripathi36@gmail.com>
2025-01-17 15:53:12 +0530

    Changes

--- src/component/News.js
+++ src/component/News.js
@@ -70,21 +70,22 @@ export class News extends Component {
         }
     }
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/everything?q=tesla&from=2024-12-17&sortBy=publishedAt&apiKey=2fdb0b08af084481b1cd2c226739c437";
+        // let url = "https://newsapi.org/v2/everything?q=tesla&from=2024-12-17&sortBy=publishedAt&apiKey=2fdb0b08af084481b1cd2c226739c437";
+        let url = "https://gnews.io/api/v4/search?q=example&apikey=eba93223e002ccd7034540fc52e2fdaa";
         let data = await fetch(url);
         let filterData = await data.json();
         console.log('filterData ::::',filterData);
         if (filterData && filterData.articles && filterData.articles.length) {
-            this.setState({ article: filterData.articles, totalResults: filterData.totalResults });
+            this.setState({ article: filterData.articles, totalResults: filterData.totalArticles });
         }
     }
     privious = async () => {
         console.log('privious');
-        let url = `https://newsapi.org/v2/everything?q=tesla&from=2024-12-17&sortBy=publishedAt&apiKey=2fdb0b08af084481b1cd2c226739c437&page=${this.state.page - 1}&pageSize=9`;
+        let url = `https://gnews.io/api/v4/search?q=example&apikey=eba93223e002ccd7034540fc52e2fdaa&page=${this.state.page - 1}`;
         let data = await fetch(url);
         let filterData = await data.json();
-        console.log(filterData);
         if (filterData && filterData.articles && filterData.articles.length) {
+            console.log(filterData);
             this.setState({ article: filterData.articles, });
             this.setState({
                 page: this.state.page - 1
@@ -93,14 +94,14 @@ export class News extends Component {
     }
     next = async () => {
         console.log('next');
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 9)) {
+        if (this.state.page + 1 > Math.ceil(this.state.totalArticles / 9)) {
         }
         else {
-            let url = `https://newsapi.org/v2/everything?q=tesla&from=2024-12-17&sortBy=publishedAt&apiKey=2fdb0b08af084481b1cd2c226739c437&page=${this.state.page + 1}&pageSize=9`;
+            let url = `https://gnews.io/api/v4/search?q=example&apikey=eba93223e002ccd7034540fc52e2fdaa&page=${this.state.page + 1}`;
             let data = await fetch(url);
             let filterData = await data.json();
-            console.log(filterData);
             if (filterData && filterData.articles && filterData.articles.length) {
+                console.log(filterData);
                 this.setState({ article: filterData.articles });
                 this.setState({
                     page: this.state.page + 1
@@ -124,13 +125,13 @@ export class News extends Component {
                         <h1 className='text-center'>BaBa Hot News</h1>
                         {this.state.article && this.state.article.length && this.state.article.map((e) => {
                             return <div className='col-md-4 ' key={e.url} >
-                                <NewsItem title={e.title ? e.title.slice(0, 40) : ""} discription={e.description ? e.description.slice(0, 80) : ""} imgUrl={e.urlToImage} newsUrl={e.url} />
+                                <NewsItem title={e.title ? e.title.slice(0, 40) : ""} discription={e.description ? e.description.slice(0, 80) : ""} imgUrl={e.image} newsUrl={e.url} />
                             </div>
                         })}
                     </div>
                     <div className="container d-flex justify-content-between my-5">
                         <button disabled={this.state.page <= 1} className='btn btn-dark' onClick={this.privious}>&larr; Previous </button>
-                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 9)} className='btn btn-dark' onClick={this.next}>Next &rarr; </button>
+                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / 10)} className='btn btn-dark' onClick={this.next}>Next &rarr; </button>
                     </div>
                 </div>
             </>
